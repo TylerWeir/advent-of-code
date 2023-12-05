7d98a8d667c08b81ca039557e6bab3022d792aae
Tyler Weir <tyler.weir@firstmode.com>
2023-12-04 19:37:54 -0800

    refactor(day01): clean up tests and parsing

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,38 +1,22 @@
+/// Solution for Advent of Code 2023, day 1.
+///
+/// Refactor heavily supported by: https://www.youtube.com/watch?v=JOgQMjpGum0
+
 /// Returns a number constructed from the first and last digits
-///
-/// The returned number is constructed such that the first digit in the string populates the tens
-/// place and the last digit populates the ones place. Strings containing a single digit will
-/// populate both the ones and the tens place with the digit. This function will return None for
-/// strings without a digit.
-///
-/// # Arguments
-///
-/// * `input` - a string slice to parse the number from
 ///
 /// # Examples
 ///
 /// - `'abc1sd3'` -> `Some(13)`
 /// - `'fhcg5s'` -> `Some(55)`
-/// - `'powasf'` -> None
-///
-fn parse_line(input: &str) -> Option<u32> {
-    let mut first: Option<char> = None;
-    let mut last: Option<char> = None;
+fn parse_line(input: &str) -> u32 {
+    let mut nums = input.chars().filter(|c| c.is_digit(10));
 
-    for c in input.chars() {
-        if c.is_digit(10) {
-            match first {
-                None => first.replace(c),
-                Some(_) => last.replace(c),
-            };
-        };
-    }
+    let first = nums.next().expect("Input must contain at least one digit.");
+    let last = nums.last();
 
-    let nums = (first, last);
-    match nums {
-        (Some(a), Some(b)) => Some(a.to_digit(10).unwrap() * 10 + b.to_digit(10).unwrap()),
-        (Some(a), None) => Some(a.to_digit(10).unwrap() * 10 + a.to_digit(10).unwrap()),
-        (None, _) => None,
+    match (first, last) {
+        (f, Some(l)) => format!("{f}{l}").parse().expect("Failed to parse."),
+        (f, None) => format!("{f}{f}").parse().expect("Failed to parse."),
     }
 }
 
@@ -41,17 +25,13 @@ fn parse_line(input: &str) -> Option<u32> {
 /// # Arguments
 ///
 /// * `input` - `'\n'` separated string slice to parse calibration values from
-fn calc_calibration_sum(input: &str) -> u32 {
-    input
-        .split('\n')
-        .map(parse_line)
-        .map(|a| a.unwrap_or(0))
-        .sum()
+fn solve(input: &str) -> u32 {
+    input.lines().map(parse_line).sum()
 }
 
 fn main() {
     let input = include_str!("./input.txt");
-    print!("{}\n", calc_calibration_sum(input));
+    print!("{}\n", solve(input));
 }
 
 #[cfg(test)]
@@ -59,30 +39,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn no_digit() {
-        assert_eq!(parse_line("hello"), None);
-        assert_eq!(parse_line(""), None);
-    }
-
-    #[test]
-    fn one_digit() {
-        assert_eq!(parse_line("treb7uchet"), Some(77));
-    }
-
-    #[test]
-    fn two_digits() {
-        assert_eq!(parse_line("1abc2"), Some(12));
-    }
-
-    #[test]
-    fn many_digits() {
-        assert_eq!(parse_line("pqr3stu8vwx"), Some(38));
-        assert_eq!(parse_line("a1b2c3d4e5f"), Some(15));
+    fn line_parser() {
+        assert_eq!(parse_line("treb7uchet"), 77);
+        assert_eq!(parse_line("1abc2"), 12);
+        assert_eq!(parse_line("pqr3stu8vwx"), 38);
+        assert_eq!(parse_line("a1b2c3d4e5f"), 15);
     }
 
     #[test]
     fn full_puzzle_input() {
         let input = include_str!("./input.txt");
-        assert_eq!(calc_calibration_sum(input), 54877);
+        assert_eq!(solve(input), 54877);
     }
 }
